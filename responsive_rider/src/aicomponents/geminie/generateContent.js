088cd20ef084c9e6c12907cdaddd3bd40f186152
Rahul Waghmare <rahulwaghmare1711@gmail.com>
2025-02-06 13:45:51 +0530

    ai genrated quizzes done

--- responsive_rider/src/aicomponents/geminie/generateContent.js
+++ responsive_rider/src/aicomponents/geminie/generateContent.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 // Function to generate content using Google Generative AI
 export async function generateContent(prompt) {
   try {
-    const genAI = new GoogleGenerativeAI("AIzaSyBqOh40tH8QTxsiSaifBiH3n68qjWMaTAg");
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(prompt);
